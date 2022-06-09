060c5e860b0a83989057a9dff973b1ad6cce78e6
lokesh <lokeshns7476@gmail.com>
2022-06-09 22:07:39 +0530

    8th commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-mo5s2jzy3z59vuzzsi-t79k#t06wwt+2t#$_&$(uj*@5w4yl*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -73,8 +73,8 @@ DATABASES = {
         'NAME': 'my_db3',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
+        'USER': 'root',
+        'PASSWORD': '7476',
     }
 }
 
@@ -120,8 +120,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'],
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD'],
+EMAIL_HOST_USER = 'lokeshns7476loki@gmail.com'
+EMAIL_HOST_PASSWORD = 'elsyxrrjcewbqqut'
 
 
 # Default primary key field type
