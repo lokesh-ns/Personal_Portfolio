2d38050a14e1b100ff212a55741006e07c893438
lokesh <lokeshns7476@gmail.com>
2022-06-09 18:29:07 +0530

    third commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mo5s2jzy3z59vuzzsi-t79k#t06wwt+2t#$_&$(uj*@5w4yl*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://lokeshns-portfolio.herokuapp.com/", "127.0.0.1"]
 
 
 # Application definition
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
