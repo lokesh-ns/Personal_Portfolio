8a126b634cbaee183acdef74cf482b0f5d2cb64c
lokesh <lokeshns7476@gmail.com>
2022-06-09 19:33:03 +0530

    7th commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mo5s2jzy3z59vuzzsi-t79k#t06wwt+2t#$_&$(uj*@5w4yl*p'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -73,8 +73,8 @@ DATABASES = {
         'NAME': 'my_db3',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '7476',
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
     }
 }
 
@@ -120,8 +120,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lokeshns7476loki@gmail.com'
-EMAIL_HOST_PASSWORD = 'elsyxrrjcewbqqut'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'],
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD'],
 
 
 # Default primary key field type
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from home_app import views as views_home
 from about_app import views as views_about
 from skill_app import views as views_skills
 from project_app import views as views_projects
@@ -11,7 +10,6 @@ from contact_app import views as views_contact
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
-    # path('home/', views_home.home, name='home'),
     path('about/', views_about.about, name='about'),
     path('skill/', views_skills.skill, name='skill'),
     path('project/', views_projects.project, name='project'),
