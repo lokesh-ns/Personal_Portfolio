f8fad81f0f760d47e377cd33ee01afb8a701d031
lokesh-ns <lokeshns7476loki@gmail.com>
2023-05-26 15:47:23 +0530

    fixed static and template directory issues

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -69,12 +69,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my_db3',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '7476',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
